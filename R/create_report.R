9086907b4084796501fed304877b451231babf26
Johannes Griss <jgriss8@yahoo.com>
2020-01-20 14:39:16 +0100

    Added information about analysis settings to report PDF

--- R/create_report.R
+++ R/create_report.R
@@ -6,6 +6,8 @@
 #' @param result ReactomeAnalysisResult. The result to create the report for.
 #' @param pdf_filename character. Path where the PDF file will be written to. This file
 #'        will be overwritten if it exists
+#' @param include_disease bool. Indicates whether disease pathways were included in the analysis.
+#' @param include_interactors bool. Indicates whether interactors where included in the analysis.
 #'
 #' @export
 #'
@@ -18,7 +20,8 @@
 #'
 #' # create the PDF report at the specified location (simply a temporary file in this example)
 #' create_pdf_report(result = griss_melanoma_result, pdf_filename = pdf_file_path)
-create_pdf_report <- function(result, pdf_filename) {
+create_pdf_report <- function(result, pdf_filename, include_disease = NA, include_interactors = NA) {
   markdown_file <- system.file("gsa_report.rmd", package = "ReactomeGSA.report")
-  rmarkdown::render(markdown_file, output_file = pdf_filename, params = list(result = result))
+  rmarkdown::render(markdown_file, output_file = pdf_filename, params = list(
+    result = result, include_disease = include_disease, include_interactors = include_interactors))
 }
