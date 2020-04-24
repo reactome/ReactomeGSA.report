48ad04a06c951f5954717799fb19ead35eb8db87
Johannes Griss <jgriss8@yahoo.com>
2020-04-24 14:45:33 +0200

    Added GSVA reports

--- R/create_report.R
+++ R/create_report.R
@@ -21,7 +21,12 @@
 #' # create the PDF report at the specified location (simply a temporary file in this example)
 #' create_pdf_report(result = griss_melanoma_result, pdf_filename = pdf_file_path)
 create_pdf_report <- function(result, pdf_filename, include_disease = NA, include_interactors = NA) {
-  markdown_file <- system.file("gsa_report.rmd", package = "ReactomeGSA.report")
+  if (ReactomeGSA:::is_gsva_result(result)) {
+    markdown_file <- system.file("gsva_report.rmd", package = "ReactomeGSA.report")
+  } else {
+    markdown_file <- system.file("gsa_report.rmd", package = "ReactomeGSA.report")
+  }
+
   rmarkdown::render(markdown_file, output_file = pdf_filename, params = list(
     result = result, include_disease = include_disease, include_interactors = include_interactors))
 }
